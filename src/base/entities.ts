6e140ec7134baa5f174a7230e0b7cdbbb9e06452
SID37 <1999login@mail.ru>
2020-05-15 02:49:55 +0300

    implemented physics interfaces

--- src/base/entities.ts
+++ src/base/entities.ts
@@ -1,28 +1,46 @@
-import {Point} from "geometry";
+import {Point} from "./geometry";
 import {DrawableActor, DrawableMap} from "../graphics/drawer";
-import {PhisicsActor} from "../phisics/phisics";
+import {PhisicsActor, PhisicsMap} from "../phisics/phisics";
+import {extractImageData} from "../image/helpers";
 
-export class BlankCar implements DrawableActor {
+export class BlankCar implements DrawableActor, PhisicsActor {
     angle: number;
     coordinates: Point;
     speed: number;
     necessary_speed: number;
     wheel_angle: number;
 
+    public get height(){
+        return this.view.height;
+    }
+
+    public get width(){
+        return this.view.width;
+    }
+
     public constructor(readonly view: ImageBitmap) {
         this.angle = 0;
-        this.coordinates = new Point(500, 500);
+        this.coordinates = new Point(0, 0);
         this.speed = 0;
         this.necessary_speed = 10;
         this.wheel_angle = 1;
     }
 }
 
-export class BlankMap implements DrawableMap {
+export class BlankMap implements DrawableMap, PhisicsMap {
     public constructor(readonly stage: ImageBitmap, public car: BlankCar) {
     }
 
-    public actors(): Array<DrawableActor> {
+    is_barrier(p: Point): boolean{
+        let img = extractImageData(this.stage);
+        if (p.x < 0 || p.y < 0 || p.x > img.width || p.y > img.height) {
+            return true
+        }
+        let offset = (p.x + p.y * img.width) * 4;
+        return 255 !== [0, 1, 2].map(i => img.data[offset + i]).reduce((a, b) => Math.min(a, b))
+    }
+
+    public actors() {
         return [this.car];
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,12 @@
 import {Drawer} from "./graphics/drawer";
 import {BlankCar, BlankMap} from "./base/entities";
-import {downloadBitmap, extractImageData} from "./image/helpers";
+import {downloadBitmap} from "./image/helpers";
 import {Point} from "./base/geometry";
-
-class PhysicsMap {
-    constructor(private img: ImageData) {
-    }
-
-    is_barrier(p: Point): boolean {
-        if (p.x < 0 || p.y < 0 || p.x > this.img.width || p.y > this.img.height) {
-            return true
-        }
-        let offset = (p.x + p.y * this.img.width) * 4;
-        return 255 !== [0, 1, 2].map(i => this.img.data[offset + i]).reduce((a, b) => Math.min(a, b))
-    }
-}
 
 let canvas = document.getElementById('main') as HTMLCanvasElement;
 let drawer = new Drawer(canvas);
 Promise.all(["map.png", "car.png"].map(downloadBitmap))
     .then(function ([mapBitmap, carBitmap]) {
-        const phMap = new PhysicsMap(extractImageData(mapBitmap));
         let map = new BlankMap(mapBitmap, new BlankCar(carBitmap));
         map.car.coordinates.x = 500;
         map.car.coordinates.y = 500;
